71befa1a21670b1613b434f352e27b196ae33504
blueMac <bluebluebluelan@gmail.com>
2016-04-14 11:56:39 +0900

    fix bug

--- sudokuvalidator.c
+++ sudokuvalidator.c
@@ -4,32 +4,21 @@
 #include <pthread.h>
 #include <string.h>
 
-#define NUM_THREADS 27
 #define LENGTH 9
-#define ONE_THIRD_LENGTH 3
-#define FALSE 0
-#define TRUE 1
-#define MIN_ARGC 2
-#define FILE_PATH_ARG 1
-#define TEST_TYPE 0
-#define START_VAL 1
+#define BLOCK_LENGTH 3
 
-
-static int *puz[LENGTH];
-static int lastRegion = 0;
-static int duplicate = FALSE;
-
+static int *solution[LENGTH];
 
 int main(int argc, const char *argv[]) {
 	
-	int i, j, k;
+	int i, j, k, l;
 
 	if (argc < 2) {
 		printf("no input\n");
 		return 0;
 	}
 	for (i = 0; i < LENGTH; i++)
-		puz[i] = (int *)malloc(sizeof(int) * LENGTH);
+		solution[i] = (int *)malloc(sizeof(int) * LENGTH);
 	
 	FILE *file;
 	
@@ -38,7 +27,7 @@ int main(int argc, const char *argv[]) {
 
 		for (i = 0; i < LENGTH; i++) {
 
-			int *ptr = puz[i];
+			int *ptr = solution[i];
 			fscanf(file, "%d %d %d %d %d %d %d %d %d", ptr, ptr+1, ptr+2, ptr+3, ptr+4, ptr+5, ptr+6, ptr+7, ptr+8);
 		}
 		printf("File Parsed.\n");
@@ -47,19 +36,12 @@ int main(int argc, const char *argv[]) {
 	}
 
 	int val1, val2;
-/*
-	for (i = 0; i < LENGTH; i++){
-		for (j = 0; j < LENGTH; j++){
-			printf("%d ", puz[i][j]);
-		}
-		printf("\n");
-	}
-*/
+
 	for (k = 0;k< LENGTH; k++){
 		for (i = 0; i < LENGTH; i++) {
-				val1 = puz[k][i];
+				val1 = solution[k][i];
 				for (j = 0; j < LENGTH; j++) {
-					val2 = puz[k][j];
+					val2 = solution[k][j];
 					if (val1 == val2) {		
 						if (!(i == j))
 						{
@@ -74,9 +56,9 @@ int main(int argc, const char *argv[]) {
 
 	for (k = 0;k< LENGTH; k++){
 		for (i = 0; i < LENGTH; i++) {
-				val1 = puz[i][k];
+				val1 = solution[i][k];
 				for (j = 0; j < LENGTH; j++) {
-					val2 = puz[j][k];
+					val2 = solution[j][k];
 					if (val1 == val2) {		
 						if (!(i == j))
 						{
@@ -87,18 +69,19 @@ int main(int argc, const char *argv[]) {
 			}
 		}
 	}
+
 	printf("row down\n");
-	int l;
-	for (k = 0; k < ONE_THIRD_LENGTH; k++){
-		for (l = 0; l < ONE_THIRD_LENGTH; l++){
+
+	for (k = 0; k < BLOCK_LENGTH; k++){
+		for (l = 0; l < BLOCK_LENGTH; l++){
 			int clonearr[9]={1,2,3,4,5,6,7,8,9};
-			val1=puz[k*3][l*3];
+			val1=solution[k*3][l*3];
 			if(clonearr[val1-1]!=0){
 				clonearr[val1-1]=0;
 			}
-			for (i = 0; i < ONE_THIRD_LENGTH; i++){
-				for (j = 0; j < ONE_THIRD_LENGTH; j++){
-					val2=puz[k*3+i][l*3+j];
+			for (i = 0; i < BLOCK_LENGTH; i++){
+				for (j = 0; j < BLOCK_LENGTH; j++){
+					val2=solution[k*3+i][l*3+j];
 					if(clonearr[val2-1]!=0){
 						clonearr[val2-1]=0;
 					}
@@ -116,11 +99,9 @@ int main(int argc, const char *argv[]) {
 	printf("block down\n");
 
 	
- 	/* Free memory for the columns in each row */
  	for (i = 0; i < LENGTH; i++)
- 		free(puz[i]);
+ 		free(solution[i]);
 	
 	printf("done\n");
-	// return with success status
 	return 0;
 }
